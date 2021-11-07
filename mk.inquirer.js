d3777bd947c92ad27bcfc4b2bc251a0e57e0b25f
chathu2686 <chathu2686@hotmail.com>
2021-11-07 16:45:36 +0000

    started setting up inquirer file

---
+++ mk.inquirer.js
@@ -0,0 +1,11 @@
+const { Fighter, Battle, Autobattle } = require("./mk");
+const fighterDb = require("./fighterDb");
+
+const questions = [
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your name?",
+  },
+  {},
+];
